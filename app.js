068ffd8a714dadd9616664243baa87f0cc305a67
Nicholas Ferrara <ferraranicholas@yahoo.com>
2017-03-01 20:50:36 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 document.getElementById("openFile").addEventListener('change', function(){
 	var fr = new FileReader();
 	fr.onload = function(evt){
-		document.getElementById("Filecontents").textContent = this.result;
-
+		//document.getElementById("Filecontents").textContent = this.result;
 		//call back
+		
 		var contents = evt.target.result;
+		console.log("Content:", contents);
 
 	}
 	fr.readAsText(this.files[0]);
-	console.log(contents);
+	//console.log(contents);
 })
